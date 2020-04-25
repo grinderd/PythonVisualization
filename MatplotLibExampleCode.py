2d089b613c2455f92a39f1dd338e5650e89848fe
David Grinder <grinderd@gmail.com>
2020-04-24 20:18:07 -0400

    Working through Python for Data science.  Starting plotly examples and notes

---
+++ MatplotLibExampleCode.py
@@ -0,0 +1,8 @@
+#=================================================#
+#
+#Purpose: Document examples in matplotlib or seaborn visualization
+#   - Through other study identify some examples of matplot lib plots and options that I want to keep with explanation
+#
+#
+#
+#=================================================#
---
+++ PythonforDataAnalysisNotes.py
@@ -0,0 +1,11 @@
+#Purpose: Notes from chapter 9 of Python for Data analysis.  Also will work to make example code work
+
+
+#9.1
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.arange(10)
+
+plt.plot(data)
